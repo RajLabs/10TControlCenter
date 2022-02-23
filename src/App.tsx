ddd0912050cd48e5994c00a58ddc814d2d8cd57f
Deva <deva@zufiz.com>
2022-02-23 18:08:31 +0530

    added husky

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
+
+
           <code>src/App.tsx</code>
           and save to reload.
         </p>
