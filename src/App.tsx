603e0f1d69a5b79e41aa19d779c52729ffb4380a
Deva <deva@zufiz.com>
2022-02-23 18:22:45 +0530

    testing husky

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
-
-
           <code>src/App.tsx</code>
           and save to reload.
         </p>
