44e42b59eeca804de52ed9e485e2af761726a71b
deva <deva@zufiz.com>
2022-03-04 19:04:38 +0530

    Changed Data grid , search bar, selector in All location

--- src/modules/dashboard/location/LocationDataTable.tsx
+++ src/modules/dashboard/location/LocationDataTable.tsx
@@ -1,7 +1,16 @@
+import { ArrowDownward } from '@mui/icons-material';
+import SearchIcon from '@mui/icons-material/Search';
+import {
+  Autocomplete,
+  Button,
+  Grid,
+  IconButton,
+  InputBase,
+  Stack,
+  TextField
+} from '@mui/material';
 import Box from '@mui/material/Box';
-import Checkbox from '@mui/material/Checkbox';
 import Paper from '@mui/material/Paper';
-import { alpha } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,92 +18,150 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
 import Toolbar from '@mui/material/Toolbar';
-import { visuallyHidden } from '@mui/utils';
 import * as React from 'react';
+import styles from './Location.module.css';
 
 interface Data {
-  calories: number;
-  carbs: number;
-  fat: number;
-  name: string;
-  protein: number;
+  location: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: number;
+  status: string;
+  emg: string;
 }
 
 function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
+  location: string,
+  address: string,
+  city: string,
+  state: string,
+  zip: number,
+  status: string,
+  emg: string
 ): Data {
   return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein
+    location,
+    address,
+    city,
+    state,
+    zip,
+    status,
+    emg
   };
 }
 
 const rows = [
-  createData('Starbucks(4605)', 305, 3.7, 67, 4.3),
-  createData('Starbucks(4605)', 452, 25.0, 51, 4.9),
-  createData('Starbucks(4605)', 262, 16.0, 24, 6.0),
-  createData('Starbucks(4605)', 159, 6.0, 24, 4.0),
-  createData('Starbucks(4605)', 356, 16.0, 49, 3.9),
-  createData('Starbucks(4605)', 408, 3.2, 87, 6.5),
-  createData('Starbucks(4605)', 237, 9.0, 37, 4.3),
-  createData('Starbucks(4605)', 375, 0.0, 94, 0.0),
-  createData('Starbucks(4605)', 518, 26.0, 65, 7.0),
-  createData('Starbucks(4605)', 392, 0.2, 98, 0.0),
-  createData('Starbucks(4605)', 318, 0, 81, 2.0),
-  createData('Starbucks(4605)', 360, 19.0, 9, 37.0),
-  createData('Starbucks(4605)', 437, 18.0, 63, 4.0)
+  createData(
+    'Starbucks(4605 E Chandler Blvd-phonenix,AZ)',
+    '564 G.A.R HighWay',
+    'Phoneix',
+    'AZ',
+    45344,
+    'online',
+    'off'
+  ),
+  createData(
+    'Starbucks(4605 E Chandler Blvd-phonenix,AZ)',
+    '564 G.A.R HighWay',
+    'Phoneix',
+    'AZ',
+    45344,
+    'offline',
+    'off'
+  ),
+  createData(
+    'Starbucks(4605 E Chandler Blvd-phonenix,AZ)',
+    '564 G.A.R HighWay',
+    'Phoneix',
+    'AZ',
+    45344,
+    'online',
+    'off'
+  ),
+  createData(
+    'Starbucks(4605 E Chandler Blvd-phonenix,AZ)',
+    '564 G.A.R HighWay',
+    'Phoneix',
+    'AZ',
+    45344,
+    'offline',
+    'off'
+  ),
+  createData(
+    'Starbucks(4605 E Chandler Blvd-phonenix,AZ)',
+    '564 G.A.R HighWay',
+    'Phoneix',
+    'AZ',
+    45344,
+    'online',
+    'off'
+  ),
+  createData(
+    'Starbucks(4605 E Chandler Blvd-phonenix,AZ)',
+    '564 G.A.R HighWay',
+    'Phoneix',
+    'AZ',
+    45344,
+    'Trouble',
+    'off'
+  ),
+  createData(
+    'Starbucks(4605 E Chandler Blvd-phonenix,AZ)',
+    '564 G.A.R HighWay',
+    'Phoneix',
+    'AZ',
+    45344,
+    'Trouble',
+    'off'
+  ),
+  createData(
+    'Starbucks(4605 E Chandler Blvd-phonenix,AZ)',
+    '564 G.A.R HighWay',
+    'Phoneix',
+    'AZ',
+    45344,
+    'online',
+    'off'
+  ),
+  createData(
+    'Starbucks(4605 E Chandler Blvd-phonenix,AZ)',
+    '564 G.A.R HighWay',
+    'Phoneix',
+    'AZ',
+    45344,
+    'online',
+    'off'
+  ),
+  createData(
+    'Starbucks(4605 E Chandler Blvd-phonenix,AZ)',
+    '564 G.A.R HighWay',
+    'Phoneix',
+    'AZ',
+    45344,
+    'online',
+    'off'
+  ),
+  createData(
+    'Starbucks(4605 E Chandler Blvd-phonenix,AZ)',
+    '564 G.A.R HighWay',
+    'Phoneix',
+    'AZ',
+    45344,
+    'online',
+    'off'
+  ),
+  createData(
+    'Starbucks(4605 E Chandler Blvd-phonenix,AZ)',
+    '564 G.A.R HighWay',
+    'Phoneix',
+    'AZ',
+    45344,
+    'online',
+    'off'
+  )
 ];
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-type Order = 'asc' | 'desc';
-
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key
-): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string }
-) => number {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
-function stableSort<T>(
-  array: readonly T[],
-  comparator: (a: T, b: T) => number
-) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map(el => el[0]);
-}
-
 interface HeadCell {
   disablePadding: boolean;
   id: keyof Data;
@@ -104,96 +171,83 @@ interface HeadCell {
 
 const headCells: readonly HeadCell[] = [
   {
-    id: 'name',
-    numeric: false,
-    disablePadding: true,
-    label: 'Location'
-  },
-  {
-    id: 'calories',
+    id: 'address',
     numeric: true,
     disablePadding: false,
     label: 'Address'
   },
   {
-    id: 'fat',
+    id: 'city',
     numeric: true,
     disablePadding: false,
     label: 'City'
   },
   {
-    id: 'carbs',
+    id: 'state',
+    numeric: true,
+    disablePadding: false,
+    label: 'State'
+  },
+  {
+    id: 'zip',
     numeric: true,
     disablePadding: false,
     label: 'Zip'
   },
   {
-    id: 'protein',
+    id: 'status',
+    numeric: true,
+    disablePadding: false,
+    label: 'Status'
+  },
+  {
+    id: 'emg',
     numeric: true,
     disablePadding: false,
     label: 'e911'
   }
 ];
 
-interface EnhancedTableProps {
-  numSelected: number;
-  onRequestSort: (
-    event: React.MouseEvent<unknown>,
-    property: keyof Data
-  ) => void;
-  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  order: Order;
-  orderBy: string;
-  rowCount: number;
-}
-
-function EnhancedTableHead(props: EnhancedTableProps) {
-  const {
-    onSelectAllClick,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort
-  } = props;
-  const createSortHandler =
-    (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
-      onRequestSort(event, property);
-    };
-
+const names = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder'
+];
+function EnhancedTableHead() {
   return (
-    <TableHead>
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              'aria-label': 'select all desserts'
-            }}
-          />
+    <TableHead className={styles.tableHeader}>
+      <TableRow className={styles.tableHeader}>
+        <TableCell
+          sx={{
+            backgroundColor: '#D3E3FC',
+            paddingLeft: '20px',
+            minWidth: 200,
+            fontWeight: 900
+          }}
+        >
+          Location Name
+          <IconButton>
+            <ArrowDownward style={{ fontSize: 18, fontWeight: 900 }} />
+          </IconButton>
         </TableCell>
         {headCells.map(headCell => (
           <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
-            padding={headCell.disablePadding ? 'none' : 'normal'}
-            sortDirection={orderBy === headCell.id ? order : false}
+            sx={{
+              backgroundColor: '#D3E3FC',
+              fontWeight: 900
+            }}
           >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </Box>
-              ) : null}
-            </TableSortLabel>
+            {headCell.label}
+            <IconButton>
+              <ArrowDownward style={{ fontSize: 18, fontWeight: 900 }} />
+            </IconButton>
           </TableCell>
         ))}
       </TableRow>
@@ -201,76 +255,81 @@ function EnhancedTableHead(props: EnhancedTableProps) {
   );
 }
 
-interface EnhancedTableToolbarProps {
-  numSelected: number;
-}
-
-function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
-  const { numSelected } = props;
-
+function EnhancedTableToolbar() {
   return (
-    <Toolbar
-      sx={{
-        pl: { sm: 2 },
-        pr: { xs: 1, sm: 1 },
-        ...(numSelected > 0 && {
-          bgcolor: theme =>
-            alpha(
-              theme.palette.primary.main,
-              theme.palette.action.activatedOpacity
-            )
-        })
-      }}
-    >
-      <div style={{ fontSize: 1 }}>s</div>
+    <Toolbar>
+      <Grid container spacing={1} sx={{ marginTop: '20px' }}>
+        <Grid item xs={10} md={10} lg={10}>
+          <span>Filter</span>
+          <InputBase
+            className={styles.locationSearch}
+            placeholder="Location Name / Address"
+            inputProps={{ 'aria-label': 'search' }}
+          />
+          <IconButton
+            className={styles.locationSearchIcon}
+            type="submit"
+            aria-label="search"
+          >
+            <SearchIcon />
+          </IconButton>
+          <div className={styles.city}>
+            <Autocomplete
+              id="size-small-outlined"
+              size="small"
+              options={names}
+              getOptionLabel={option => option}
+              renderInput={params => (
+                <TextField
+                  {...params}
+                  placeholder="City"
+                  sx={{ marginTop: '-7px' }}
+                />
+              )}
+            />
+          </div>
+          <Stack
+            spacing={2}
+            sx={{
+              width: 100,
+              display: 'inline-block',
+              mx: 1
+            }}
+          >
+            <Autocomplete
+              id="size-small-outlined"
+              size="small"
+              options={names}
+              getOptionLabel={option => option}
+              renderInput={params => (
+                <TextField
+                  {...params}
+                  placeholder="State"
+                  sx={{ marginTop: '-7px' }}
+                />
+              )}
+            />
+          </Stack>
+          <Button variant="contained" style={{ backgroundColor: '#5346A0' }}>
+            +Add
+          </Button>
+        </Grid>
+        <Grid item xs={2} md={2} lg={2}>
+          <Button
+            variant="contained"
+            style={{ backgroundColor: 'gray', borderRadius: '20px' }}
+          >
+            +Add Location
+          </Button>
+        </Grid>
+      </Grid>
     </Toolbar>
   );
 }
 
 export default function LocationDataTable() {
-  const [order, setOrder] = React.useState<Order>('asc');
-  const [orderBy, setOrderBy] = React.useState<keyof Data>('calories');
-  const [selected, setSelected] = React.useState<readonly string[]>([]);
   const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const handleRequestSort = (
-    event: React.MouseEvent<unknown>,
-    property: keyof Data
-  ) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const newSelecteds = rows.map(n => n.name);
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected: readonly string[] = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
-    }
-
-    setSelected(newSelected);
-  };
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
@@ -282,61 +341,60 @@ export default function LocationDataTable() {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
-
-  const isSelected = (name: string) => selected.indexOf(name) !== -1;
-
-  // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
   return (
     <Box sx={{ width: '100%' }}>
       <Paper sx={{ width: '100%', mb: 2 }}>
-        <EnhancedTableToolbar numSelected={selected.length} />
-        <TableContainer>
+        <EnhancedTableToolbar />
+        <TableContainer
+          sx={{ paddingLeft: '25px', paddingRight: '25px', marginTop: '20px' }}
+        >
           <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
-            <EnhancedTableHead
-              numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
-              rowCount={rows.length}
-            />
+            <EnhancedTableHead />
             <TableBody>
-              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
-              rows.slice().sort(getComparator(order, orderBy)) */}
-              {stableSort(rows, getComparator(order, orderBy))
+              {rows
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
-                  const isItemSelected = isSelected(row.name);
                   const labelId = `enhanced-table-checkbox-${index}`;
-
                   return (
                     <TableRow
                       hover
-                      onClick={event => handleClick(event, row.name)}
                       role="checkbox"
-                      aria-checked={isItemSelected}
                       tabIndex={-1}
-                      key={row.name}
-                      selected={isItemSelected}
+                      key={row.location}
                     >
-                      <TableCell>
-                        <Checkbox />
-                      </TableCell>
                       <TableCell
                         component="th"
                         id={labelId}
                         scope="row"
                         padding="none"
+                        align="left"
+                        sx={{ padding: '20px' }}
                       >
-                        {row.name}
+                        {row.location}
                       </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
+                      <TableCell align="left">{row.address}</TableCell>
+                      <TableCell align="left">{row.city}</TableCell>
+                      <TableCell align="left">{row.state}</TableCell>
+                      <TableCell align="left">{row.zip}</TableCell>
+                      {row.status === 'online' ? (
+                        <TableCell align="left">
+                          <div className={styles.online}> {row.status}</div>
+                        </TableCell>
+                      ) : null}
+                      {row.status === 'offline' ? (
+                        <TableCell align="left">
+                          <div className={styles.offline}> {row.status}</div>
+                        </TableCell>
+                      ) : null}
+                      {row.status === 'Trouble' ? (
+                        <TableCell align="left">
+                          <div className={styles.trouble}> {row.status}</div>
+                        </TableCell>
+                      ) : null}
+                      <TableCell align="left">{row.emg}</TableCell>
                     </TableRow>
                   );
                 })}
@@ -349,7 +407,7 @@ export default function LocationDataTable() {
           </Table>
         </TableContainer>
         <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
+          rowsPerPageOptions={[10, 15, 20]}
           component="div"
           count={rows.length}
           rowsPerPage={rowsPerPage}
